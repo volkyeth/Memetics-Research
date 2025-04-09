267df3db15cdd9a06764f9ceffbe51b0ba41f114
emergentvibe <yiannis.rav@gmail.com>
2025-04-09 11:42:47 +0100

    update quartz config

--- source/quartz.config.ts
+++ source/quartz.config.ts
@@ -16,7 +16,7 @@ const config: QuartzConfig = {
       provider: "plausible",
     },
     locale: "en-US",
-    baseUrl: "quartz.jzhao.xyz",
+    baseUrl: "research.emergentvibe.com",
     ignorePatterns: ["private", "templates", ".obsidian"],
     defaultDateType: "created",
     theme: {
